09781861f8cbdf7426ff8f2cd9c5fb64669bbd30
Manya Bondada <manya.bondada@gmail.com>
2023-04-23 16:30:52 -0500

    search bar added

--- hobby hub/src/pages/ReadPosts.jsx
+++ hobby hub/src/pages/ReadPosts.jsx
@@ -5,7 +5,8 @@ import { supabase } from '../client'
 const ReadPosts = () => {
 
     const [posts, setPosts] = useState([]);
-
+    const [search, setSearch] = useState('');
+ 
     useEffect(() => {
         fetchPosts();
     }, []);
@@ -16,13 +17,21 @@ const ReadPosts = () => {
         .select()
         .order('upvotes', { ascending: false })
         setPosts(data);
-      }
+    }
+
+    const filteredPosts = posts.filter((post) =>
+      post.title.toLowerCase().includes(search.toLowerCase())
+    );
     
     return (
         <div className="ReadPosts">
+            <div className="search">
+                <input type="text" placeholder="Search by title" value={search} onChange={(e) => setSearch(e.target.value)} />
+            </div>
+            <br/>
             {
-                posts && posts.length > 0 ?
-                posts.map((post,index) => 
+                filteredPosts && filteredPosts.length > 0 ?
+                filteredPosts.map((post,index) => 
                    <SummaryCard id={post.id} created_at={Math.floor((Date.now() - Date.parse(post.created_at)) / (1000 * 60 * 60))} title={post.title} upvotes={post.upvotes} key={index}/>
                 ) : <h2>No Posts Yet. Click to Add!</h2>
             }
