3249018e7a5d5a9d47d5c83284fbf35540eb8f1c
Manya Bondada <manya.bondada@gmail.com>
2023-04-23 17:22:25 -0500

    order by feature fixed

--- hobby hub/src/pages/ReadPosts.jsx
+++ hobby hub/src/pages/ReadPosts.jsx
@@ -6,6 +6,9 @@ const ReadPosts = () => {
 
     const [posts, setPosts] = useState([]);
     const [search, setSearch] = useState('');
+    const [sortBy, setSortBy] = useState('upvotes');
+    // if sort popular, ascending=false, if sort newest, ascending=true
+    const [orderBy, setOrderBy] = useState(false);
  
     useEffect(() => {
         fetchPosts();
@@ -15,20 +18,41 @@ const ReadPosts = () => {
         const {data} = await supabase
         .from('Avatarposts')
         .select()
-        .order('upvotes', { ascending: false })
+        .order(sortBy, { ascending: orderBy })
         setPosts(data);
     }
 
-    const filteredPosts = posts.filter((post) =>
+    const sortedPosts = [...posts].sort((a, b) => { // sorts based on vote/date and re renders feed 
+        if (sortBy) {
+          if(a[sortBy] == 0)
+            return a[sortBy]
+          else
+            return a[sortBy] - b[sortBy]; // a needs to go before b
+        } else {
+          return b[sortBy] - a[sortBy]; // b needs to go before a
+        }
+      });
+
+    const filteredPosts = sortedPosts.filter((post) => // looks through sorted posts to see if title exists
       post.title.toLowerCase().includes(search.toLowerCase())
     );
     
     return (
         <div className="ReadPosts">
-            <div className="search">
-                <input type="text" placeholder="Search by title" value={search} onChange={(e) => setSearch(e.target.value)} />
+            
+            <div className="function">
+                <button className="orderTime" onClick={() => {
+                    setSortBy('upvotes');
+                    setOrderBy(!sortBy);
+                }}>Newest</button>
+                <button className="orderUp" onClick={() => {
+                    setSortBy('created_at');
+                    setORderBy(!sortBy);
+                }}>Popular</button>
+                <input className="search" type="text" placeholder="Search by title" value={search} onChange={(e) => setSearch(e.target.value)} />
             </div>
             <br/>
+
             {
                 filteredPosts && filteredPosts.length > 0 ?
                 filteredPosts.map((post,index) => 
